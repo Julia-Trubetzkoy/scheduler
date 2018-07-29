a2858b815be11648a2b17ee5153c7a17973a448d
Julia Trubetzkoy <jigglebuttthinks11@gmail.com>
2018-07-28 20:13:08 -0600

    installed and connected redux devtools to application and used spread operator to set payload data in store

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -10,7 +10,7 @@ class LibraryCourse extends Component {
             <div className="library-course">
             <div className="library-course__title-arrow">
                 <label className="library-course__title">Problem Solving</label>
-                { Icon('fas fa-plus-circle', 'library-course__icon') }
+                { Icon('fas fa-check', 'library-course__icon') }
                 </div>
                 <Arrow className="library-course__arrow" />
                 <Action className="library-course__action"/>
