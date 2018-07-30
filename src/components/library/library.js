e232be81c373deb5c220de99d99908455d0f2eac
Julia Trubetzkoy <jigglebuttthinks11@gmail.com>
2018-07-29 19:58:36 -0600

    implemented mapStateToProps and mapped over data to return the amount of course component instances we require

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -7,8 +7,16 @@ import LibraryCourse from './libraryCourse';
 
 class Library extends Component {
 
-    componentDidMount() {
+    componentWillMount() {
         this.props.fetchCourses()
+    }
+
+    renderCourses() {
+        const data = this.props.fetchCourses
+
+        return data.map((LibraryCourse, index) => {
+            return <LibraryCourse key={index}/>
+        })
     }
 
     render() {
@@ -18,9 +26,16 @@ class Library extends Component {
                 <LibraryCourse/>
                 <LibraryCourse/>
                 <LibraryCourse/>
+                { this.renderCourses() }
             </div>
         )
     }
 }
 
-export default connect(null, actions)(Library);
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps, actions)(Library);
